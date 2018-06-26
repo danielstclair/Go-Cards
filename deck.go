d8ebb0b48899440c31614d6b14ffca18793ee3c7
danielstclair <danielallanstclair@gmail.com>
2018-06-26 10:56:49 -0500

    feat(Deck): Save Deck to file
    Add functionality for saving a Deck to local file.

--- deck.go
+++ deck.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 // Create a new type of 'deck'
@@ -31,4 +33,13 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+// toString would make more sense as a receiver
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
